c85851c4bbf86109b66d3a8d614d78a2066d0768
najwanaribena <semuatentanggadget@gmail.com>
2022-10-10 11:17:02 +0700

    android RelativeLayout2

--- android/RelativeLayout2/app/src/main/java/com/example/relativelayout2/MainActivity.java
+++ android/RelativeLayout2/app/src/main/java/com/example/relativelayout2/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.relativelayout;
+package com.example.relativelayout2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_relative);
+        setContentView(R.layout.activity_main);
     }
 }
