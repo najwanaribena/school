ceb4aff47762c7de7aecead1be40f41e31f9e3c5
najwanaribena <semuatentanggadget@gmail.com>
2022-11-07 10:46:20 +0700

    android UHP2Intent

---
+++ android/UHP2Intent/app/src/main/java/com/example/uhp2intent/IntentActivity.java
@@ -0,0 +1,28 @@
+package com.example.uhp2intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class IntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent);
+        EditText editText2 = findViewById(R.id.editext2);
+        Button get2 = findViewById(R.id.get2);
+
+        get2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(IntentActivity.this, "Welcome " + editText2.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
---
+++ android/UHP2Intent/app/src/main/java/com/example/uhp2intent/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.uhp2intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button get1 = findViewById(R.id.get1);
+
+        get1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, IntentActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
